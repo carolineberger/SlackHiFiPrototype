b1bca82e8cd927fa1e4e5a996682d3b967e52d17
carolineberger <caroline.berger2@mail.mcgill.ca>
2017-11-11 12:31:13 -0500

    maven project startup, add socket

--- src/ch/swingfx/twinkle/HiFiPrototypeScheme.java
+++ src/ch/swingfx/twinkle/HiFiPrototypeScheme.java
@@ -13,7 +13,7 @@ import ch.swingfx.twinkle.window.Positions;
 
 public class HiFiPrototypeScheme {
 	
-	public static void play(){
+	public static void play(String text){
 				INotificationStyle style = new LightDefaultNotification()
 				.withWidth(400) // Optional
 				.withAlpha(0.9f) // Optional
@@ -32,7 +32,7 @@ public class HiFiPrototypeScheme {
 		new NotificationBuilder()
 		.withStyle(style2) // Required. here we set the previously set style
 		.withTitle("Henry (Boss): (Channel: PROJECTS)") // Required.
-		.withMessage("Does Greg have the spreadsheet?") // Optional
+		.withMessage(text) // Optional
 		.withIcon(new ImageIcon(QuickStart.class.getResource("/ch/swingfx/twinkle/resource/Boss.jpg")))
 		
 		.withNotificationManager(NotificationManagers.SEQUENTIAL)
@@ -53,180 +53,181 @@ public class HiFiPrototypeScheme {
 		
 		.showNotification(); // this returns a UUID that you can use to identify events on the listener
 		
-		
-		
-		INotificationStyle style3 = new BlueDefaultNotification()
-				.withWidth(400) // Optional
-				.withAlpha(0.9f) // Optional
-		;
-		new NotificationBuilder()
-		.withStyle(style3) // Required. here we set the previously set style
-		.withTitle("Antoine (Spanish Meetup): (Channel: PUBCRAWL)") // Required.
-		.withMessage("Guys, who's ready for tonight?") // Optional
-		.withIcon(new ImageIcon(QuickStart.class.getResource("/ch/swingfx/twinkle/resource/Antoine.jpg")))
-		
-		.withNotificationManager(NotificationManagers.SEQUENTIAL)
-		.withDisplayTime(3000) // Optional
-		.withPosition(Positions.NORTH_EAST) // Optional. Show it at the center of the screen
-		.withListener(new NotificationEventAdapter() { // Optional
-			public void closed(NotificationEvent event) {
-				System.out.println("closed notification with UUID " + event.getId());
-			}
-
-			public void clicked(NotificationEvent event) {
-		        
-				System.out.println("clicked notification with UUID " + event.getId());
-		            
-		       
-			}
-		})
-		
-		.showNotification(); // this returns a UUID that you can use to identify events on the listener
-		
-		new NotificationBuilder()
-		.withStyle(style2) // Required. here we set the previously set style
-		.withTitle("Greg (co-worker): (Channel: PROJECTS)") // Required.
-		.withMessage("Yep, can @everyone take a look?") // Optional
-		.withIcon(new ImageIcon(QuickStart.class.getResource("/ch/swingfx/twinkle/resource/Greg.jpg")))
-		.withNotificationManager(NotificationManagers.SEQUENTIAL)
-		
-		//.withIcon(new ImageIcon(QuickStart.class.getResource("/twinkle.png"))) // Optional. You could also use a String path
-		.withDisplayTime(3000) // Optional
-		.withPosition(Positions.NORTH_EAST) // Optional. Show it at the center of the screen
-		.withListener(new NotificationEventAdapter() { // Optional
-			public void closed(NotificationEvent event) {
-				System.out.println("closed notification with UUID " + event.getId());
-			}
-
-			public void clicked(NotificationEvent event) {
-		        
-				System.out.println("clicked notification with UUID " + event.getId());
-		            
-		       
-			}
-		})
-		.showNotification(); // this returns a UUID that you can use to identify events on the listener
-
-		new NotificationBuilder()
-		.withStyle(style2) // Required. here we set the previously set style
-		.withTitle("Henry (Boss): (Channel: PROJECTS)") // Required.
-		.withMessage("Please do this by the end of the week @everyone") // Optional
-		.withIcon(new ImageIcon(QuickStart.class.getResource("/ch/swingfx/twinkle/resource/Boss.jpg")))
-		
-		.withNotificationManager(NotificationManagers.SEQUENTIAL)
-		.withDisplayTime(3000) // Optional
-		.withPosition(Positions.NORTH_EAST) // Optional. Show it at the center of the screen
-		.withListener(new NotificationEventAdapter() { // Optional
-			public void closed(NotificationEvent event) {
-				System.out.println("closed notification with UUID " + event.getId());
-			}
-
-			public void clicked(NotificationEvent event) {
-		        
-				System.out.println("clicked notification with UUID " + event.getId());
-		            
-		       
-			}
-		})
-		
-		.showNotification(); // this returns a UUID that you can use to identify events on the listener
-		
-		new NotificationBuilder()
-		.withStyle(style) // Required. here we set the previously set style
-		.withTitle("Sarah (co-worker): (Channel: RANDOM)") // Required.
-		.withMessage("Who's going to the dinner tonight?") // Optional
-		.withIcon(new ImageIcon(QuickStart.class.getResource("/ch/swingfx/twinkle/resource/Sarah.jpg")))
-		.withNotificationManager(NotificationManagers.SEQUENTIAL)
-		
-		//.withIcon(new ImageIcon(QuickStart.class.getResource("/twinkle.png"))) // Optional. You could also use a String path
-		.withDisplayTime(3000) // Optional
-		.withPosition(Positions.NORTH_EAST) // Optional. Show it at the center of the screen
-		.withListener(new NotificationEventAdapter() { // Optional
-			public void closed(NotificationEvent event) {
-				System.out.println("closed notification with UUID " + event.getId());
-			}
-
-			public void clicked(NotificationEvent event) {
-		        
-				System.out.println("clicked notification with UUID " + event.getId());
-		            
-		       
-			}
-		})
-		.showNotification(); // this returns a UUID that you can use to identify events on the listener
-
-		
-		new NotificationBuilder()
-		.withStyle(style3) // Required. here we set the previously set style
-		.withTitle("Mark (Spanish Meetup): (Channel: PUBCRAWL)") // Required.
-		.withMessage("Yes 100% I can't wait dude. What time are we meeting?") // Optional
-		.withIcon(new ImageIcon(QuickStart.class.getResource("/ch/swingfx/twinkle/resource/Mark.jpg")))
-		
-		.withNotificationManager(NotificationManagers.SEQUENTIAL)
-		.withDisplayTime(3000) // Optional
-		.withPosition(Positions.NORTH_EAST) // Optional. Show it at the center of the screen
-		.withListener(new NotificationEventAdapter() { // Optional
-			public void closed(NotificationEvent event) {
-				System.out.println("closed notification with UUID " + event.getId());
-			}
-
-			public void clicked(NotificationEvent event) {
-		        
-				System.out.println("clicked notification with UUID " + event.getId());
-		            
-		       
-			}
-		})
-		
-		.showNotification(); // this returns a UUID that you can use to identify events on the listener
-		new NotificationBuilder()
-		.withStyle(style3) // Required. here we set the previously set style
-		.withTitle("Antoine (Spanish Meetup): (Channel: PUBCRAWL)") // Required.
-		.withMessage("@Mark around 8 PM") // Optional
-		.withIcon(new ImageIcon(QuickStart.class.getResource("/ch/swingfx/twinkle/resource/Antoine.jpg")))
-		
-		.withNotificationManager(NotificationManagers.SEQUENTIAL)
-		.withDisplayTime(3000) // Optional
-		.withPosition(Positions.NORTH_EAST) // Optional. Show it at the center of the screen
-		.withListener(new NotificationEventAdapter() { // Optional
-			public void closed(NotificationEvent event) {
-				System.out.println("closed notification with UUID " + event.getId());
-			}
-
-			public void clicked(NotificationEvent event) {
-		        
-				System.out.println("clicked notification with UUID " + event.getId());
-		            
-		       
-			}
-		})
-		
-		.showNotification(); // this returns a UUID that you can use to identify events on the listener
-		
-		new NotificationBuilder()
-		.withStyle(style) // Required. here we set the previously set style
-		.withTitle("Sarah (co-worker): (Channel: DIRECT MESSAGE)") // Required.
-		.withMessage("OMG I found the best sushi place last night!") // Optional
-		.withIcon(new ImageIcon(QuickStart.class.getResource("/ch/swingfx/twinkle/resource/Sarah.jpg")))
-		.withNotificationManager(NotificationManagers.SEQUENTIAL)
-		
-		//.withIcon(new ImageIcon(QuickStart.class.getResource("/twinkle.png"))) // Optional. You could also use a String path
-		.withDisplayTime(3000) // Optional
-		.withPosition(Positions.NORTH_EAST) // Optional. Show it at the center of the screen
-		.withListener(new NotificationEventAdapter() { // Optional
-			public void closed(NotificationEvent event) {
-				System.out.println("closed notification with UUID " + event.getId());
-			}
-
-			public void clicked(NotificationEvent event) {
-		        
-				System.out.println("clicked notification with UUID " + event.getId());
-		            
-		       
-			}
-		})
-		.showNotification(); // this returns a UUID that you can use to identify events on the listener
+//		
+//		
+//		INotificationStyle style3 = new BlueDefaultNotification()
+//				.withWidth(400) // Optional
+//				.withAlpha(0.9f) // Optional
+//		;
+//		new NotificationBuilder()
+//		.withStyle(style3) // Required. here we set the previously set style
+//		.withTitle("Antoine (Spanish Meetup): (Channel: PUBCRAWL)") // Required.
+//		.withMessage("Guys, who's ready for tonight?") // Optional
+//		.withIcon(new ImageIcon(QuickStart.class.getResource("/ch/swingfx/twinkle/resource/Antoine.jpg")))
+//		
+//		.withNotificationManager(NotificationManagers.SEQUENTIAL)
+//		.withDisplayTime(3000) // Optional
+//		.withPosition(Positions.NORTH_EAST) // Optional. Show it at the center of the screen
+//		.withListener(new NotificationEventAdapter() { // Optional
+//			public void closed(NotificationEvent event) {
+//				System.out.println("closed notification with UUID " + event.getId());
+//			}
+//
+//			public void clicked(NotificationEvent event) {
+//		        
+//				System.out.println("clicked notification with UUID " + event.getId());
+//		            
+//		       
+//			}
+//		})
+//		
+//		.showNotification(); // this returns a UUID that you can use to identify events on the listener
+//		
+//		new NotificationBuilder()
+//		.withStyle(style2) // Required. here we set the previously set style
+//		.withTitle("Greg (co-worker): (Channel: PROJECTS)") // Required.
+//		.withMessage("Yep, can @everyone take a look?") // Optional
+//		.withIcon(new ImageIcon(QuickStart.class.getResource("/ch/swingfx/twinkle/resource/Greg.jpg")))
+//		.withNotificationManager(NotificationManagers.SEQUENTIAL)
+//		
+//		//.withIcon(new ImageIcon(QuickStart.class.getResource("/twinkle.png"))) // Optional. You could also use a String path
+//		.withDisplayTime(3000) // Optional
+//		.withPosition(Positions.NORTH_EAST) // Optional. Show it at the center of the screen
+//		.withListener(new NotificationEventAdapter() { // Optional
+//			public void closed(NotificationEvent event) {
+//				System.out.println("closed notification with UUID " + event.getId());
+//			}
+//
+//			public void clicked(NotificationEvent event) {
+//		        
+//				System.out.println("clicked notification with UUID " + event.getId());
+//		            
+//		       
+//			}
+//		})
+//		.showNotification(); // this returns a UUID that you can use to identify events on the listener
+//
+//		new NotificationBuilder()
+//		.withStyle(style2) // Required. here we set the previously set style
+//		.withTitle("Henry (Boss): (Channel: PROJECTS)") // Required.
+//		.withMessage("Please do this by the end of the week @everyone") // Optional
+//		.withIcon(new ImageIcon(QuickStart.class.getResource("/ch/swingfx/twinkle/resource/Boss.jpg")))
+//		
+//		.withNotificationManager(NotificationManagers.SEQUENTIAL)
+//		.withDisplayTime(3000) // Optional
+//		.withPosition(Positions.NORTH_EAST) // Optional. Show it at the center of the screen
+//		.withListener(new NotificationEventAdapter() { // Optional
+//			public void closed(NotificationEvent event) {
+//				System.out.println("closed notification with UUID " + event.getId());
+//			}
+//
+//			public void clicked(NotificationEvent event) {
+//		        
+//				System.out.println("clicked notification with UUID " + event.getId());
+//		            
+//		       
+//			}
+//		})
+//		
+//		.showNotification(); // this returns a UUID that you can use to identify events on the listener
+//		
+//		new NotificationBuilder()
+//		.withStyle(style) // Required. here we set the previously set style
+//		.withTitle("Sarah (co-worker): (Channel: RANDOM)") // Required.
+//		.withMessage("Who's going to the dinner tonight?") // Optional
+//		.withIcon(new ImageIcon(QuickStart.class.getResource("/ch/swingfx/twinkle/resource/Sarah.jpg")))
+//		.withNotificationManager(NotificationManagers.SEQUENTIAL)
+//		
+//		//.withIcon(new ImageIcon(QuickStart.class.getResource("/twinkle.png"))) // Optional. You could also use a String path
+//		.withDisplayTime(3000) // Optional
+//		.withPosition(Positions.NORTH_EAST) // Optional. Show it at the center of the screen
+//		.withListener(new NotificationEventAdapter() { // Optional
+//			public void closed(NotificationEvent event) {
+//				System.out.println("closed notification with UUID " + event.getId());
+//			}
+//
+//			public void clicked(NotificationEvent event) {
+//		        
+//				System.out.println("clicked notification with UUID " + event.getId());
+//		            
+//		       
+//			}
+//		})
+//		.showNotification(); // this returns a UUID that you can use to identify events on the listener
+//
+//		
+//		new NotificationBuilder()
+//		.withStyle(style3) // Required. here we set the previously set style
+//		.withTitle("Mark (Spanish Meetup): (Channel: PUBCRAWL)") // Required.
+//		.withMessage("Yes 100% I can't wait dude. What time are we meeting?") // Optional
+//		.withIcon(new ImageIcon(QuickStart.class.getResource("/ch/swingfx/twinkle/resource/Mark.jpg")))
+//		
+//		.withNotificationManager(NotificationManagers.SEQUENTIAL)
+//		.withDisplayTime(3000) // Optional
+//		.withPosition(Positions.NORTH_EAST) // Optional. Show it at the center of the screen
+//		.withListener(new NotificationEventAdapter() { // Optional
+//			public void closed(NotificationEvent event) {
+//				System.out.println("closed notification with UUID " + event.getId());
+//			}
+//
+//			public void clicked(NotificationEvent event) {
+//		        
+//				System.out.println("clicked notification with UUID " + event.getId());
+//		            
+//		       
+//			}
+//		})
+//		
+//		.showNotification(); // this returns a UUID that you can use to identify events on the listener
+//		new NotificationBuilder()
+//		.withStyle(style3) // Required. here we set the previously set style
+//		.withTitle("Antoine (Spanish Meetup): (Channel: PUBCRAWL)") // Required.
+//		.withMessage("@Mark around 8 PM") // Optional
+//		.withIcon(new ImageIcon(QuickStart.class.getResource("/ch/swingfx/twinkle/resource/Antoine.jpg")))
+//		
+//		.withNotificationManager(NotificationManagers.SEQUENTIAL)
+//		.withDisplayTime(3000) // Optional
+//		.withPosition(Positions.NORTH_EAST) // Optional. Show it at the center of the screen
+//		.withListener(new NotificationEventAdapter() { // Optional
+//			public void closed(NotificationEvent event) {
+//				System.out.println("closed notification with UUID " + event.getId());
+//			}
+//
+//			public void clicked(NotificationEvent event) {
+//		        
+//				System.out.println("clicked notification with UUID " + event.getId());
+//		            
+//		       
+//			}
+//		})
+//		
+//		.showNotification(); // this returns a UUID that you can use to identify events on the listener
+//		
+//		new NotificationBuilder()
+//		.withStyle(style) // Required. here we set the previously set style
+//		.withTitle("Sarah (co-worker): (Channel: DIRECT MESSAGE)") // Required.
+//		.withMessage("OMG I found the best sushi place last night!") // Optional
+//		.withIcon(new ImageIcon(QuickStart.class.getResource("/ch/swingfx/twinkle/resource/Sarah.jpg")))
+//		.withNotificationManager(NotificationManagers.SEQUENTIAL)
+//		
+//		//.withIcon(new ImageIcon(QuickStart.class.getResource("/twinkle.png"))) // Optional. You could also use a String path
+//		.withDisplayTime(3000) // Optional
+//		.withPosition(Positions.NORTH_EAST) // Optional. Show it at the center of the screen
+//		.withListener(new NotificationEventAdapter() { // Optional
+//			public void closed(NotificationEvent event) {
+//				System.out.println("closed notification with UUID " + event.getId());
+//			}
+//
+//			public void clicked(NotificationEvent event) {
+//		        
+//				System.out.println("clicked notification with UUID " + event.getId());
+//		            
+//		       
+//			}
+//		})
+//		.showNotification(); // this returns a UUID that you can use to identify events on the listener
+//
+//	}
 
 	}
-
 }
--- src/ch/swingfx/twinkle/QuickStart.java
+++ src/ch/swingfx/twinkle/QuickStart.java
@@ -1,52 +1,47 @@
 package ch.swingfx.twinkle;
 
 
+import java.net.URISyntaxException;
+
 import ch.swingfx.twinkle.event.NotificationEvent;
 import ch.swingfx.twinkle.event.NotificationEventAdapter;
 import ch.swingfx.twinkle.style.INotificationStyle;
 import ch.swingfx.twinkle.style.theme.LightDefaultNotification;
 import ch.swingfx.twinkle.window.Positions;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class QuickStart {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws URISyntaxException {
 		// AA the text
 		System.setProperty("swing.aatext", "true");
-
+		Socket socket = IO.socket("http://localhost:3001");
+		socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+			public void call(Object... arg0) {
+				// TODO Auto-generated method stub
+				System.out.println("connected to server");
+			}
+		})
+		.on("notify", new Emitter.Listener() {
+			
+			public void call(Object... arg0) {
+				// TODO Auto-generated method stub
+				HiFiPrototypeScheme.play((String) arg0[0]);
+			}
+		});
+		
+		socket.connect();
 		// First we define the style/theme of the window.
 		// Note how we override the default values
-		INotificationStyle style = new LightDefaultNotification()
-				.withWidth(400) // Optional
-				.withAlpha(0.9f) // Optional
-		;
-
-		// Now lets build the notification
-		
-		new NotificationBuilder()
-				.withStyle(style) // Required. here we set the previously set style
-				.withTitle("Starting Simulation in 10 Seconds") // Required.
-				.withDisplayTime(10000)
-				.withPosition(Positions.CENTER) // Optional. Show it at the center of the screen
-				.withListener(new NotificationEventAdapter() { // Optional
-					public void closed(NotificationEvent event) {
-						System.out.println("closed notification with UUID " + event.getId());
-					}
-
-					public void clicked(NotificationEvent event) {
-						System.out.println("clicked notification with UUID " + event.getId());
-				            
-				       
-					}
-				})
-				.showNotification(); // this returns a UUID that you can use to identify events on the listener
+	
 		
 		
 		
-		HiFiPrototypeScheme.play();
 		//NoColorScheme.play();
 		
 			
 
 	}
-
 }
